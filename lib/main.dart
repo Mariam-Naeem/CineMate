55ca4903899c88fdd4a03f04f84bf522b2ffda45
Mariam naeem saeed <mariam77naeem@gmail.com>
2026-09-19 01:30:51 +0300

    first screen initial

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,22 @@
+import 'package:flutter/material.dart';
 
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Cinemate',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Cinemate'),
+        ),
+        body: Center(
+          child: Text('Welcome to Cinemate!'),
+        ),
+      ),
+    );
+  }
+}
